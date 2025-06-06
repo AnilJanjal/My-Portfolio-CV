bd234a67bac0a6c979cc227c774d8cb8159ffcf1
Anil Janjale <ajjanjal2000@gmail.comm>
2025-06-06 22:08:19 +0530

    u

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,5 @@
-import project1 from "../assets/projects/project-1.png";
-import project2 from "../assets/projects/project-2.png";
-import project3 from "../assets/projects/project-3.jpg";
+import project from "../assets/projects/project.png";
+
 
 export const HERO_CONTENT = `Driven to build scalable and efficient applications, with a commitment to
 delivering high-quality, user-centric solutions while continuously improving
@@ -27,35 +26,35 @@ export const EXPERIENCES = [
 export const PROJECTS = [
   {
     title: "SoftBuild (Inventory Management System)",
-    image: project2,
+    image: project,
     description:
       "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
     technologies: ["ReactJS", "Firebase", "HTML", "CSS", "React", "Router", "Context API", "Git"],
   },
   {
     title: "LearnCraft (Course Selling Platform)",
-    image: project1,
+    image: project,
     description:
       "LearnCraft is a static course-selling website designed for instructors (clients) to upload pre-recorded video courses and set prices for students to purchase. the platform operates as a client-students system without any online learning environment. If focuses solely on selling video-based courses efficiently.",
     technologies: ["HTML", "CSS", "React", "Node.js", "Express.js", " Socket.IO", "MongoDB"],
   },
   {
     title: "Bewakoof Clone (E-commerce Site)",
-    image: project1,
+    image: project,
     description:
       "Cloned UI of popular fashion e-commerce platform with shopping cart, filtering, and product listing features.",
     technologies: ["HTML", "CSS", "React", "Node.js", "Express.js"],
   },
   {
     title: "MediMizer (Hospital Management System)",
-    image: project3,
+    image: project,
     description:
       "MediMizer help hospitals and others who need Hospital Management Solutions for BioMedical Compliance, From customer support to tracking parts to tracking medical equipment our software helps you filter down to the finest detail to get the data for decision making.",
     technologies: ["HTML", "React", "Axios","Node.js + Express.js", "Bootstrap","MongoDB", "JWT", "RESTful APIs" ,"CI/CD" ,"AWS"],
   },
   {
     title: "FieldServio (Field Service Management Tool)",
-    image: project1,
+    image: project,
     description:
       "FieldServio helps you manage it all, whether it be customer management, inventory control, mobile field operations, or financial reporting.",
     technologies: ["HTML", "Tailwind CSS", "React", "Node.js", "Express.js", " Socket.IO", "MongoDB", "AWS","CI/CD" , "Postman" ],
